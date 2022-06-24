e6c6bda1f7e62c4307e009f5ea09ee8c487ecb4f
Diego Gabriel León Peña <diegabo1996@yahoo.es>
2022-06-24 13:24:15 -0400

    Agregar archivos de proyecto.

---
+++ FunctionsMethods/Models/Bulb.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionsMethods.Models
+{
+    public class Bulb
+    {
+        private string Color { get; set; }
+        private byte Watts { get; set; }
+        private byte Voltage { get; set; }
+        private bool State { get; set; }
+
+        //Method
+        private void TurnOn()
+        {
+            State = true;
+            Color = "Amarillo";
+            Voltage = 220;
+            Watts = 100;
+        }
+        //Method
+        private void TurnOff()
+        {
+            State = false;
+            Color = "Ninguno";
+            Voltage = 0;
+            Watts = 0;
+        }
+        //Method
+        public void PressSwitch()
+        {
+            if (State == true)
+            {
+                TurnOff();
+            }
+            else
+            {
+                TurnOn();
+            }
+        }
+        //Function
+        public bool StateBulb()
+        {
+            return State;
+        }
+        public byte WattsBulb()
+        { 
+            return Watts; 
+        }
+        public byte VoltageBulb()
+        { 
+            return Voltage; 
+        }
+        public string ColorBulb()
+        {
+            return Color;
+        }
+    }
+}
---
+++ FunctionsMethods/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using FunctionsMethods.Models;
+
+namespace FunctionsMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Bulb bombilla = new Bulb();
+
+            ShowBulbData(bombilla);
+            bombilla.PressSwitch();
+            ShowBulbData(bombilla);
+            bombilla.PressSwitch();
+            ShowBulbData(bombilla);
+
+        }
+        public static  void ShowBulbData(Bulb bombilla)
+        {
+            Console.WriteLine("Estado de Bombilla: " + bombilla.StateBulb());
+            Console.WriteLine("Watts de Bombilla: " + bombilla.WattsBulb());
+            Console.WriteLine("Voltage de Bombilla: " + bombilla.VoltageBulb());
+            Console.WriteLine("Color de Bombilla: " + bombilla.ColorBulb());
+        }
+    }
+}
